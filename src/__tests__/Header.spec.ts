8a50db57276f85bf01bfd4c0ea4afdee115a709b
John Cheng <cheng.john@pm.me>
2021-12-19 14:27:58 +0200

    test: Remove header tests

--- src/__tests__/Header.spec.ts
+++
@@ -1,8 +0,0 @@
-import '@testing-library/jest-dom';
-import { render } from '@testing-library/svelte';
-import Header from '../lib/Header.svelte';
-
-it('renders correctly', () => {
-	const { container } = render(Header);
-	expect(container).toMatchSnapshot();
-});
