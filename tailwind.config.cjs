f290c2a6cfd4c764b06e20efb8b324602c4af96a
John Cheng <cheng.john@pm.me>
2021-12-19 16:04:53 +0200

    style: Add design system colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,13 @@
 module.exports = {
 	content: ['./src/**/*.{svelte,html}'],
 	theme: {
+		colors: {
+			white: '#FFFFFF',
+			beige: '#FDF9F3',
+			salmon: '#FFC8B0',
+			green: '#163A45',
+			black: '#041621'
+		},
 		extend: {}
 	},
 	plugins: []
