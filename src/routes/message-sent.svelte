424622cfe5cfb9d57db3047c351abc9bafef56cd
John Cheng <cheng.john@pm.me>
2022-01-23 11:42:07 +0200

    style: Add padding and center text

--- src/routes/message-sent.svelte
+++ src/routes/message-sent.svelte
@@ -2,8 +2,8 @@
 	<title>Koodikatu - Thank you page</title>
 </svelte:head>
 
-<section class="prose lg:prose-xl grid justify-items-center">
-	<p>Thank you for reaching out to me!</p>
+<section class="px-4 prose lg:prose-xl grid justify-items-center text-center">
+	<h2>Thank you for reaching out to me!</h2>
 	<p>If you left me your email, I will contact you back in 1-3 business days.</p>
 	<lottie-player
 		src="https://assets5.lottiefiles.com/packages/lf20_hqrpwcwb.json"
