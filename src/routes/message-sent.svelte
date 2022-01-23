16ba41b7eb2423a0bc0246312d27cd4add766903
John Cheng <cheng.john@pm.me>
2022-01-23 11:33:54 +0200

    feat: Add Lottie animation to message sent page

--- src/routes/message-sent.svelte
+++ src/routes/message-sent.svelte
@@ -1,4 +1,16 @@
-<section class="prose lg:prose-xl">
+<svelte:head>
+	<title>Koodikatu - Thank you page</title>
+</svelte:head>
+
+<section class="prose lg:prose-xl grid justify-items-center">
 	<p>Thank you for reaching out to me!</p>
 	<p>If you left me your email, I will contact you back in 1-3 business days.</p>
+	<lottie-player
+		src="https://assets5.lottiefiles.com/packages/lf20_hqrpwcwb.json"
+		background="transparent"
+		speed="1"
+		style="width: 300px; height: 300px;"
+		loop
+		autoplay
+	/>
 </section>
