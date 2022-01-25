a69c9cbe8066065bfd3338ae2983ab5e72241c6f
John Cheng <cheng.john@pm.me>
2022-01-26 00:58:03 +0200

    chore: Run formatter

--- src/routes/message-sent.svelte
+++ src/routes/message-sent.svelte
@@ -2,7 +2,7 @@
 	<title>Koodikatu - Thank you for your message</title>
 </svelte:head>
 
-<section class="px-4 prose lg:prose-xl grid justify-items-center text-center">
+<section class="prose grid justify-items-center px-4 text-center lg:prose-xl">
 	<h2>Thank you for reaching out to me!</h2>
 	<p>If you left me your email, I will contact you back in 1-3 business days.</p>
 	<lottie-player
@@ -11,6 +11,6 @@
 		speed="1"
 		loop
 		autoplay
-		class="w-72 h-72"
+		class="h-72 w-72"
 	/>
 </section>
